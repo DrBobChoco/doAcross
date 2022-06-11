86a6c077710ecb68f6b964b15b961879c09ad754
Andrew Wilson <drbobchoco@gmail.com>
2022-06-11 20:34:46 +0100

    Tidy initial files

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
-
 </script>
 
 <main>
-	<h1>Werd</h1>
+    <h1>Werd</h1>
 </main>
 
 <style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
 
         @media (min-width: 640px) {
-			max-width: none;
+            max-width: none;
         }
-	}
+    }
 </style>
